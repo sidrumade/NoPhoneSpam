60c86144730c03d46c4a5c8246e71bcc814aeccb
Adam Novak <interfect@gmail.com>
2021-04-20 20:15:02 -0700

    Unify dispatch for phone state updates so none are missed

--- app/src/main/java/at/bitfire/nophonespam/CallReceiver.java
+++ app/src/main/java/at/bitfire/nophonespam/CallReceiver.java
@@ -51,101 +51,110 @@ public class CallReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        if (TelephonyManager.ACTION_PHONE_STATE_CHANGED.equals(intent.getAction()) &&
-                intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_RINGING)) {
-            Settings settings = new Settings(context);
+        if (TelephonyManager.ACTION_PHONE_STATE_CHANGED.equals(intent.getAction())) {
+            String extraState = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+            Log.d(TAG, "Received phone state change to the extra state " + extraState);
 
-            if (settings.getCallBlockingMode() != BlockingModes.ALLOW_ALL) {
-
-                String incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
-
-                /* swy: we can receive two notifications; the first one doesn't
-                        have EXTRA_INCOMING_NUMBER, so just skip it */
-                if (incomingNumber == null)
-                    return;
-
-                Log.i(TAG, "Received call: " + incomingNumber);
-
-
-                if (TextUtils.isEmpty(incomingNumber)) {
-                    // private number (no caller ID)
-                    if (settings.blockHiddenNumbers())
-                        rejectCall(context, null, context.getString(R.string.receiver_notify_private_number));
-
-                }
-                // block all calls
-                else if (settings.getCallBlockingMode() == BlockingModes.BLOCK_ALL) {
-                    Log.i(TAG, "Block all Calls: " + incomingNumber);
-                    Number number;
-                    if(isNumberPresentInContacts(context, incomingNumber)){
-                        String name = getCallerID(context, incomingNumber);
-                        number = new Number(incomingNumber, name);
-                    }
-                    else{
-                        number = new Number(incomingNumber);
-                    }
-                    rejectCall(context, number,context.getString(R.string.receiver_notify_no_call_allowed));
-                }
-                // allow calls only from contacts
-                else if (settings.getCallBlockingMode() == BlockingModes.ALLOW_CONTACTS) {
-                    if (!isNumberPresentInContacts(context, incomingNumber)) {
-                        Log.i(TAG, "Number not in contacts: " + incomingNumber);
-                        rejectCall(context, new Number(incomingNumber), context.getString(R.string.receiver_notify_not_found_in_contacts));
-                    }
-                }
-                else {
-                    DbHelper dbHelper = new DbHelper(context);
-                    try {
-                        SQLiteDatabase db = dbHelper.getWritableDatabase();
-                        Cursor c = db.query(Number._TABLE, null, "? LIKE " + Number.NUMBER, new String[]{incomingNumber}, null, null, null);
-                        boolean inList = c.moveToNext();
-                        // block calls from the numbers stored in list
-                        if (inList && settings.getCallBlockingMode() == BlockingModes.BLOCK_LIST) {
-                            Log.i(TAG, "Number was in list: " + incomingNumber);
-                            ContentValues values = new ContentValues();
-                            DatabaseUtils.cursorRowToContentValues(c, values);
-                            Number number = Number.fromValues(values);
-
-                            rejectCall(context, number, context.getString(R.string.receiver_notify_number_was_in_list));
-
-                            values.clear();
-                            values.put(Number.LAST_CALL, System.currentTimeMillis());
-                            values.put(Number.TIMES_CALLED, number.timesCalled + 1);
-                            db.update(Number._TABLE, values, Number.NUMBER + "=?", new String[]{number.number});
-
-                            BlacklistObserver.notifyUpdated();
-
-                        }
-                        // allow calls only from numbers stored in list
-                        else if (!inList && settings.getCallBlockingMode() == BlockingModes.ALLOW_ONLY_LIST_CALLS) {
-                            Log.i(TAG, "Number was not in list: " + incomingNumber);
-
-                            Number number;
-                            if(isNumberPresentInContacts(context, incomingNumber)){
-                                String name = getCallerID(context, incomingNumber);
-                                number = new Number(incomingNumber, name);
-                            }
-                            else{
-                                number = new Number(incomingNumber);
-                            }
-
-                            rejectCall(context, number, context.getString(R.string.receiver_notify_number_was_not_in_list));
-                            BlacklistObserver.notifyUpdated();
-                        }
-                        c.close();
-                    } finally {
-                        dbHelper.close();
-                    }
-                }
-
-
-            }
             /* swy: keep track of any calls that may already be happening while someone else tries to call us;
-                        we don't want to interrupt actual, running calls by mistake */
-            if (intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_OFFHOOK))
+                    we don't want to interrupt actual, running calls by mistake */
+            if (extraState.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
+                Log.d(TAG, "Setting AlreadyOnCall");
                 AlreadyOnCall = true;
-            else if (intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_IDLE))
+            }
+            else if (extraState.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
+                Log.d(TAG, "Clearing AlreadyOnCall");
                 AlreadyOnCall = false;
+            } else if (extraState.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+                Log.d(TAG, "Handling ring");
+                Settings settings = new Settings(context);
+                if (settings.getCallBlockingMode() != BlockingModes.ALLOW_ALL) {
+                    String incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+
+                    /* swy: we can receive two notifications; the first one doesn't
+                            have EXTRA_INCOMING_NUMBER, so just skip it */
+                    if (incomingNumber == null)
+                        return;
+
+                    Log.i(TAG, "Received call: " + incomingNumber);
+
+
+                    if (TextUtils.isEmpty(incomingNumber)) {
+                        // private number (no caller ID)
+                        if (settings.blockHiddenNumbers())
+                            rejectCall(context, null, context.getString(R.string.receiver_notify_private_number));
+
+                    }
+                    // block all calls
+                    else if (settings.getCallBlockingMode() == BlockingModes.BLOCK_ALL) {
+                        Log.i(TAG, "Block all Calls: " + incomingNumber);
+                        Number number;
+                        if(isNumberPresentInContacts(context, incomingNumber)){
+                            String name = getCallerID(context, incomingNumber);
+                            number = new Number(incomingNumber, name);
+                        }
+                        else{
+                            number = new Number(incomingNumber);
+                        }
+                        rejectCall(context, number,context.getString(R.string.receiver_notify_no_call_allowed));
+                    }
+                    // allow calls only from contacts
+                    else if (settings.getCallBlockingMode() == BlockingModes.ALLOW_CONTACTS) {
+                        if (!isNumberPresentInContacts(context, incomingNumber)) {
+                            Log.i(TAG, "Number not in contacts: " + incomingNumber);
+                            rejectCall(context, new Number(incomingNumber), context.getString(R.string.receiver_notify_not_found_in_contacts));
+                        }
+                    }
+                    else {
+                        DbHelper dbHelper = new DbHelper(context);
+                        try {
+                            SQLiteDatabase db = dbHelper.getWritableDatabase();
+                            Cursor c = db.query(Number._TABLE, null, "? LIKE " + Number.NUMBER, new String[]{incomingNumber}, null, null, null);
+                            boolean inList = c.moveToNext();
+                            // block calls from the numbers stored in list
+                            if (inList && settings.getCallBlockingMode() == BlockingModes.BLOCK_LIST) {
+                                Log.i(TAG, "Number was in list: " + incomingNumber);
+                                ContentValues values = new ContentValues();
+                                DatabaseUtils.cursorRowToContentValues(c, values);
+                                Number number = Number.fromValues(values);
+
+                                rejectCall(context, number, context.getString(R.string.receiver_notify_number_was_in_list));
+
+                                values.clear();
+                                values.put(Number.LAST_CALL, System.currentTimeMillis());
+                                values.put(Number.TIMES_CALLED, number.timesCalled + 1);
+                                db.update(Number._TABLE, values, Number.NUMBER + "=?", new String[]{number.number});
+
+                                BlacklistObserver.notifyUpdated();
+
+                            }
+                            // allow calls only from numbers stored in list
+                            else if (!inList && settings.getCallBlockingMode() == BlockingModes.ALLOW_ONLY_LIST_CALLS) {
+                                Log.i(TAG, "Number was not in list: " + incomingNumber);
+
+                                Number number;
+                                if(isNumberPresentInContacts(context, incomingNumber)){
+                                    String name = getCallerID(context, incomingNumber);
+                                    number = new Number(incomingNumber, name);
+                                }
+                                else{
+                                    number = new Number(incomingNumber);
+                                }
+
+                                rejectCall(context, number, context.getString(R.string.receiver_notify_number_was_not_in_list));
+                                BlacklistObserver.notifyUpdated();
+                            }
+                            c.close();
+                        } finally {
+                            dbHelper.close();
+                        }
+                    }
+
+
+                }
+            }
+            else {
+                Log.d(TAG, "Did not match " + extraState + " with " + TelephonyManager.EXTRA_STATE_RINGING + ", " + TelephonyManager.EXTRA_STATE_OFFHOOK + ", or " + TelephonyManager.EXTRA_STATE_IDLE);
+            }
         }
     }
 
